564bef8b2fb4cdaacabfcc280739668303301064
hsw6758 <1219234650@qq.com>
2018-11-23 10:33:40 +0800

    icons finish

---
+++ travelXm/src/pages/home/components/Icons.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="icons">
+    <swiper>
+      <swiper-slide v-for="(page,index) of pages" :key="index">
+        <div class="icon" v-for="item of page" :key="item.id">
+          <div class="icon-img">
+            <img class="icon-img-content" :src="item.imgUrl"/>
+          </div>
+          <p class="icon-desc">{{item.desc}}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons',
+  data () {
+    return {
+      iconList: [{
+        id: '0001',
+        imgUrl: require('./jingdian.png'),
+        desc: '景点门票'
+      },{
+        id: '0002',
+        imgUrl: require('./yiri.png'),
+        desc: '一日游'
+      },{
+        id: '0003',
+        imgUrl: require('./biyou.png'),
+        desc: '上海比游'
+      },{
+        id: '0004',
+        imgUrl: require('./haichang.png'),
+        desc: '上海海昌'
+      },{
+        id: '0005',
+        imgUrl: require('./wenquan.png'),
+        desc: '泡温泉'
+      },{
+        id: '0006',
+        imgUrl: require('./shengdi.png'),
+        desc: '打卡圣地'
+      },{
+        id: '0007',
+        imgUrl: require('./huanle.png'),
+        desc: '上海欢乐谷'
+      },{
+        id: '0008',
+        imgUrl: require('./dong.png'),
+        desc: '野生动物园'
+      },{
+        id: '0009',
+        imgUrl: require('./dishini.png'),
+        desc: '上海迪士尼'
+      },{
+        id: '00010',
+        imgUrl: require('./quan.png'),
+        desc: '全部玩乐'
+      }]
+    }
+  },
+  computed: {
+    pages() {
+      const pages = []  // 定义一个空的一维数组
+      this.iconList.forEach((item,index) => {
+        const page = Math.floor(index / 8)  // 只有0/1两个值
+        if(!pages[page]) {  /* 如果 不存在，就在下面添加一个数组，变成了二维数组*/
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~styles/varibles.styl"
+  @import "~styles/mixins.styl"
+  .icons >>> .swiper-container
+    height: 0
+    padding-bottom: 50%
+  .icon
+    overflow: hidden
+    position: relative
+    float: left
+    width: 25%
+    height: 0
+    padding-bottom: 25%
+    .icon-img
+      position: absolute
+      top: 0
+      left: 0
+      right: 0
+      bottom: .44rem
+      box-sizing: border-box
+      padding: .1rem
+      .icon-img-content
+        display: block
+        margin: 0 auto
+        height: 100%
+    .icon-desc
+      position: absolute
+      left: 0
+      right: 0
+      bottom: 0
+      height: .44rem
+      line-height: .44rem
+      text-align: center
+      color: $darkTextColor
+      ellipsis()
+</style>
---
+++ travelXm/src/pages/home/components/Swiper.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption" >
+      <swiper-slide v-for="item in swiperList" :key="item.id"><img class="swiper-img" :src="item.imgUrl"></swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: require('./1.jpg')
+      },{
+        id: '0002',
+        imgUrl: require('./2.jpg')
+      },{
+        id: '0003',
+        imgUrl: require('./3.jpg')
+      },{
+        id: '0004',
+        imgUrl: require('./4.jpg')
+      },{
+        id: '0005',
+        imgUrl: require('./5.jpg')
+      },{
+        id: '0006',
+        imgUrl: require('./6.jpg')
+      },{
+        id: '0007',
+        imgUrl: require('./7.jpg')
+      },{
+        id: '0008',
+        imgUrl: require('./8.jpg')
+      },{
+        id: '0009',
+        imgUrl: require('./9.jpg')
+      },{
+        id: '00010',
+        imgUrl: require('./10.jpg')
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #fff
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 26%
+    background: #eee
+    .swiper-img
+      width: 100%
+</style>
